262098c40f18fa88723dde53ba87e5cd30af8b1e
xinying <xinying@192.168.0.118>
2025-05-12 14:19:52 +0800

    [feat] add snippet

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@
 require("config.lazy")
 vim.opt.wrap = true
 vim.opt.list = false
+
+-- vim.opt.runtimepath:append("~/code/proj/nvim-typing-speed")
+-- local ts = require("nvim-typing-speed")
+-- ts.setup()
+-- ts.start_typing()
